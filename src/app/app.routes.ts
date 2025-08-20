e4caa8ff1ef4ec6935374eab108e551f1fe68cb3
Marwan <marwanneno1111@gmail.com>
2025-08-20 13:24:07 +0300

    Guards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/Home/home/home.component';
 import { LoginComponent } from './pages/Login/login/login.component';
+import { authGuardGuard } from './core/guards/AuthGuard/auth-guard.guard';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: '', redirectTo: 'login', pathMatch: 'full' },
-    { path: 'Seafarers', component: HomeComponent, title: 'Seafarers' },
+    { path: 'Seafarers', component: HomeComponent, title: 'Seafarers', canActivate: [authGuardGuard] },
 ];
--- src/app/core/guards/AuthGuard/auth-guard.guard.ts
+++ src/app/core/guards/AuthGuard/auth-guard.guard.ts
@@ -4,7 +4,7 @@ import { inject, PLATFORM_ID } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 
 export const authGuardGuard: CanActivateFn = (route, state) => {
-  const router = inject(Router);
+  const router  = inject(Router);
   const platformId = inject(PLATFORM_ID);
   if (isPlatformBrowser(platformId)) {
     if (localStorage.getItem('token') !== null) {
